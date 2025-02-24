63ff4dda3c8731d4ea598615bbfbfb1946a52217
Dewanggaz <zidandewangga252@gmail.com>
2025-02-24 07:38:57 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ let Bonus2 =  500000
 let Telat =  200000
 
 alert("HALO SELAMAT DATANG")
-let izin = prompt("Kamu izin berapa kali sebulan ?")
 let kehadiran = prompt("Kamu masuk berapa hari dalam sebulan ?")
+let izin = prompt("Kamu izin berapa kali sebulan ?")
 let masuk = (kehadiran*Bonus1)
-
-let massuk2 = (kehadiran*Bonus2)
+let massuk2 = (kehadiran*Bonus1+Bonus2)
+let masuk3  = (kehadiran*Bonus1+Bonus1+Bonus2)
 let sakit = (izin*Telat)
 
 
-if(kehadiran < 20){
+if(kehadiran <= 20){
     let Hasil1 =  Gaji + masuk;
     alert(`Gaji kamu bulan ini sebesar Rp${Hasil1}`)
 
-if (izin >= 1) {
+    if (izin >= 1) {
 
-alert(`Gajimu dipotong Rp ${Hasil1 - sakit}`)
+    alert(`Gajimu dipotong Rp ${Hasil1 - sakit}`)
 }
-}else if(kehadiran >= 21){
+}else if(kehadiran == 21){
 
 let Hasil2 = Gaji + massuk2
 
@@ -28,5 +28,12 @@ let Hasil2 = Gaji + massuk2
     if (izin >= 1) {
 
     alert(`Gajimu dipotong Rp ${Hasil2 - sakit}`)
+    }
+}else if (kehadiran >= 22) {
+    let Hasil3 = Gaji + masuk3
+    alert(`Gajimu adalah Rp ${Hasil3} `)
+    if (izin >= 1) {
+
+        alert(`Gajimu dipotong Rp ${Hasil3 - sakit}`)
     }
 }
